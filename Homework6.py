f726e69604861677cb916420ad342a42bb61a22e
EvgeniyE81 <165406254+EvgeniyE81@users.noreply.github.com>
2024-04-23 21:27:52 +0300

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,7 @@
+car_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for a in car_list:
+    print(" Я езжу на автомобиле", a)
+    car_count = 0
+    for cars in car_list:
+        car_count += 10
+        print('значение переменной', car_count)
